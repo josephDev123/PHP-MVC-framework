569157674a0853df0973934de4aab9fcfb7ac65a
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-07-08 16:43:18 +0100

    modified:   index.php         modified:   ../src/Application.php         modified:   ../src/Request.php         modified:   ../src/Response.php         modified:   ../src/Router.php         modified:   ../src/View.php         modified:   ../src/controller/AboutController.php;

--- public/index.php
+++ public/index.php
@@ -3,18 +3,19 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 
-use App\src\Application;
-use App\src\AboutController;
+use App\core\Application;
+use App\controller\AboutController;
 
 $app = new Application(dirname( __DIR__));
+echo '<pre>';
+var_dump($app);
+echo '</pre>';
+return;
 
 $app->router->get('/', 'home');
 
 $app->router->get('/contact', [AboutController::class, 'index']);
 
-echo '<pre>';
-// echo  $_SERVER['REQUEST_URI'];
-// print_r($_SERVER);
-echo '</pre>';
+
 
 echo $app->Run();
