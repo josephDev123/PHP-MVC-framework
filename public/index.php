1d3dc085a4c8eb0bc79bb47030c645c5e662ef52
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-09-10 13:53:16 +0100

    add all file

--- public/index.php
+++ public/index.php
@@ -5,10 +5,9 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use App\src\Application;
 use App\src\controller\AboutController;
-use App\src\Request;
+// use App\src\Request;
 
-$app = new Application(dirname( __DIR__));
-
+$app =new Application(dirname(__DIR__));
 
 $app->router->get('/', [AboutController::class, 'home']);
 
